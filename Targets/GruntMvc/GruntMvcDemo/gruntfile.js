ede3a1644ab4675c2e8b61e488005d953a398d15
xxxmatko <martin.sutka@gmail.com>
2016-05-02 13:33:44 +0200

    Grunt integration

---
+++ Targets/GruntMvc/GruntMvcDemo/gruntfile.js
@@ -0,0 +1,34 @@
+﻿module.exports = function (grunt) {
+    // Grunt configuration
+    grunt.initConfig({
+    });
+
+    // Build task
+    grunt.registerTask("build", function (target) {
+        if (!target) {
+            grunt.fail.fatal("Missing build configuration.");
+            return;
+        }
+
+        // List of default tasks for each build configuration
+        var tasks = [
+        ];
+
+        // Custom task according to custom build configurations
+        switch (target.toUpperCase()) {
+            case "DEBUG":
+                console.log("BUILD : DEBUG.");
+                break;
+            case "RELEASE":
+                console.log("BUILD : RELEASE.");
+                break;
+            default:
+                // Unknown build configuration
+                grunt.fail.fatal("Unknown build configuration '" + target + "'");
+                break;
+        }
+
+        // Run all tasks
+        grunt.task.run.apply(grunt.task, tasks);
+    });
+};
